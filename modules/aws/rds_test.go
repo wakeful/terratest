b8a4f9866835461bbe08ca7b6160d522dc8f752a
Denis O <denis.o@linux.com>
2024-05-23 16:50:12 +0100

    instance type update

--- modules/aws/rds_test.go
+++ modules/aws/rds_test.go
@@ -121,7 +121,7 @@ func TestGetRecommendedRdsInstanceTypeErrors(t *testing.T) {
 			name:                  "No instance type available for engine",
 			region:                "us-east-1",
 			databaseEngine:        "oracle-ee",
-			databaseEngineVersion: "21.0.0.0.ru-2023-07.rur-2023-07.r1",
+			databaseEngineVersion: "19.0.0.0.ru-2024-04.rur-2024-04.r1",
 			instanceTypes:         []string{"db.r5a.large"},
 		},
 		{
