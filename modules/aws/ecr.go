3b6852fdea507aae517099fbc1337734244cfdcf
Felix Friedrich <felix@fri.edri.ch>
2025-03-09 18:28:40 +0100

    Add PutECRRepoPolicyE and PutECRRepoPolicy for AWS ECR

--- modules/aws/ecr.go
+++ modules/aws/ecr.go
@@ -161,3 +161,28 @@ func GetECRRepoPolicyE(t testing.TestingT, region string, repo *types.Repository
 	}
 	return *resp.PolicyText, nil
 }
+
+// PutECRRepoPolicy puts the given policy for the given ECR repository.
+// This will fail the test and stop execution if there is an error.
+func PutECRRepoPolicy(t testing.TestingT, region string, repo *types.Repository, policy string) {
+	err := PutECRRepoLifecyclePolicyE(t, region, repo, policy)
+	require.NoError(t, err)
+}
+
+// PutECRRepoPolicyE puts the given policy for the given ECR repository.
+func PutECRRepoPolicyE(t testing.TestingT, region string, repo *types.Repository, policy string) error {
+	logger.Default.Logf(t, "Applying repo policy for repository %s in %s", *repo.RepositoryName, region)
+
+	client, err := NewECRClientE(t, region)
+	if err != nil {
+		return err
+	}
+
+	input := &ecr.SetRepositoryPolicyInput{
+		PolicyText:     &policy,
+		RepositoryName: repo.RepositoryName,
+	}
+
+	_, err = client.SetRepositoryPolicy(context.Background(), input)
+	return err
+}
