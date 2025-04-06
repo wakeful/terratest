e6eacd55003201b9f96d599d2d9b3fc5badc06d5
James Kwon <96548424+hongil0316@users.noreply.github.com>
2025-04-06 17:49:51 -0400

    add UnmarshalK8SYamlsE

--- modules/helm/template_test.go
+++ modules/helm/template_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/logger"
@@ -178,4 +179,25 @@ func TestRenderWarning(t *testing.T) {
 	var deployment appsv1.Deployment
 	UnmarshalK8SYaml(t, string(stdout), &deployment)
 	assert.Equal(t, deployment.Name, "nginx-deployment")
+}
+
+func TestRenderMultipleManifests(t *testing.T) {
+	chart, err := filepath.Abs("testdata/multiple-manifests")
+	require.NoError(t, err)
+
+	out := RenderTemplate(t, &Options{}, chart, "test", []string{})
+
+	var configs []corev1.ConfigMap
+	UnmarshalK8SYamlsE(t, out, &configs, func(v corev1.ConfigMap) bool {
+		return v.Kind == "ConfigMap"
+	})
+	require.Len(t, configs, 1)
+	assert.Equal(t, configs[0].Name, "test-configmap")
+
+	var deploys []appsv1.Deployment
+	UnmarshalK8SYamlsE(t, out, &deploys, func(v appsv1.Deployment) bool {
+		return v.Kind == "Deployment"
+	})
+	require.Len(t, deploys, 1)
+	assert.Equal(t, deploys[0].Name, "test-deployment")
 }
